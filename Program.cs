3d38f526ed295c54758512ecf25d83cccd979390
Logan Prasse <lrprasse@mail.usf.edu>
2019-09-05 23:03:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ISM4300HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue.");
+
+            Console.ReadKey(true);
+        }
+    }
+}
