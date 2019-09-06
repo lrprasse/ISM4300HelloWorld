de0c2d4897dc62487c9524a043405fc816ca6f5c
Logan Prasse <lrprasse@mail.usf.edu>
2019-09-05 23:14:47 -0400

    Added comments.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace ISM4300HelloWorld
     {
         static void Main(string[] args)
         {
+            /*
+             * This program outputs hello world to the console and waits
+             * for user input to close.
+             */
             Console.WriteLine("Hello World!");
             Console.WriteLine("Press any key to continue.");
 
             Console.ReadKey(true);
+
+            //Files uploaded to Github including a second commit with comments.
         }
     }
 }
